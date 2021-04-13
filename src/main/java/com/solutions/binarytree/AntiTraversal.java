f4544b0bb86e9a65e7d10b4fd2e5047b80b7e931
Indumathi Vellingiri <indumca@gmail.com>
2021-04-13 15:51:26 -0700

    Initial commit

---
+++ src/main/java/com/solutions/binarytree/AntiTraversal.java
@@ -0,0 +1,87 @@
+package binarytree;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class AntiTraversal {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(7);
+        root.left.left.left = new Node(10);
+        root.left.left.right = new Node(11);
+        root.right.right.left = new Node(8);
+
+       // antiClockWiseSpiral(root);
+        BinaryTree tr = new BinaryTree();
+        Node n = tr.traverseInorder(root,5);
+        System.out.println("opt:" +n.value);
+        System.out.println("PreOrder");
+        tr.traveresePreorder(root);
+
+      //  List<Integer> result = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+     //   Collections.reverse(result);
+
+   //     result.stream().forEach(new Consumer<Integer>() {
+   //         @Override
+   //         public void accept(Integer integer) {
+ //               System.out.println(integer.toString());
+ //          }
+ //       });
+       // result.stream().filter(x->result.equals(x))
+
+
+    }
+
+    public static void antiClockWiseSpiral(Node n) {
+        BinaryTree t = new BinaryTree();
+        int i = 0;
+        int j = t.height(n);
+        System.out.println(j);
+        boolean flag = false;
+        List<List<Integer>> result = traversebfs(n);
+        while (i < j) {
+            Collections.reverse(result.get(i));
+            System.out.println(result.get(i));
+            i++;
+            System.out.println(result.get(j));
+            j--;
+
+        }
+
+
+    }
+
+
+    private static List<List<Integer>> traversebfs(Node n) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<Node> qu = new LinkedList<>();
+        if (n == null) {
+            return result;
+        }
+        qu.add(n);
+        while (!qu.isEmpty()) {
+            int size = qu.size();
+            List<Integer> val = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node temp = qu.remove();
+                val.add(temp.value);
+                if (temp.left != null) {
+                    qu.add(temp.left);
+                }
+                if (temp.right != null) {
+                    qu.add(temp.right);
+                }
+            }
+            result.add(val);
+        }
+        return result;
+    }
+
+
+}
+
+
---
+++ src/main/java/com/solutions/binarytree/BinaryTree.java
@@ -0,0 +1,133 @@
+package binarytree;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BinaryTree {
+
+    Node root;
+
+    private Node addEle(Node n, int val) {
+        if (n == null) {
+            return new Node(val);
+        } else if (val == n.value) {
+            return n;
+        } else if (val < n.value) {
+            n.left = addEle(n, val);
+        } else {
+            n.right = addEle(n, val);
+        }
+        return n;
+    }
+
+    public void add(int val) {
+        root = addEle(root, val);
+    }
+
+    public boolean find(int val) {
+        return findEle(root, val);
+    }
+
+    private boolean findEle(Node n, int val) {
+        if (n == null) {
+            return false;
+        }
+
+        if (val == n.value) {
+            return true;
+        }
+
+        return (val < n.value) ? findEle(n.left, val) :
+                findEle(n.right, val);
+    }
+    public int count =0;
+    public Node traverseInorder(Node n, int k) {
+
+        if(n==null){
+            return null ;
+        }
+        Node left =  traverseInorder(n.left,k);
+        if(left!=null)
+            return left;
+        count++;
+        if(k==count){
+            System.out.println(" in k " + n.value + " ");
+            return n;
+        }
+       // System.out.println(" " + n.value + " ");
+
+
+
+         return   traverseInorder(n.right,k);
+
+    }
+
+    public void traveresePreorder(Node n) {
+         Node temp= n;
+         Node right=null;
+        Stack<Node> lst = new Stack<Node>();
+        Stack<Node> lstR = new Stack<Node>();
+        lst.add(temp);
+        if(temp.right!=null){
+            lstR.add(temp.right);
+        }
+        while (!lst.isEmpty()|| !lstR.isEmpty()) {
+           // temp=lst.firstElement();
+            if(!lst.isEmpty()) {
+                temp = lst.pop();
+            }
+                System.out.println(" " + temp.value + " ");
+
+                if (temp.right != null) {
+                    lstR.add(temp.right);
+                }
+                if (temp.left != null) {
+
+                    lst.push(temp.left);
+                }else if(!lstR.isEmpty()) {
+                    temp = lstR.pop();
+                }
+            }
+
+    }
+
+    public int height(Node n) {
+
+        if (n == null) {
+            return -1;
+        } else {
+            int l_ht = height(n.left);
+            int r_ht = height(n.right);
+            if (l_ht > r_ht) {
+                return l_ht + 1;
+            } else {
+                return r_ht + 1;
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+//Tree node
+class Node{
+    int value;
+    Node right;
+    Node left;
+    Node(int val){
+        this.value = val;
+        left=null;
+        right=null;
+    }
+
+
+}
+
+
