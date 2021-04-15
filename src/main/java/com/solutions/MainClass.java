c52d20d1a03bf2b3545cbf237dbe5c67ad37c11f
Indumathi Vellingiri <indumca@gmail.com>
2021-04-14 19:47:57 -0700

    Fixed repeated string

--- src/main/java/com/solutions/MainClass.java
+++ src/main/java/com/solutions/MainClass.java
@@ -1,9 +1,12 @@
 package com.solutions;
 
+import java.util.Arrays;
+
 class StaticComponents
 {
     static int staticVariable;
 
+    // static block
     static
     {
         System.out.println("StaticComponents SIB");
@@ -28,36 +31,58 @@ public class MainClass
     public static void main(String[] args)
     {
         //Static Members directly accessed with Class Name
-        StaticComponents.staticVariable = 20;
+        StaticComponents.staticVariable = 20; // first calls Main class static block
         StaticComponents.staticMethod();
 
-        System.out.println(repeatedString("aba",10));
+        System.out.println("val="+repeatedString("aaba",10));
     }
 
-    //not efficient
     static long repeatedString(String s, long n) {
-        int len = s.length();
-        int d = 0;
+      /*  int len = s.length();
+        int count = 0;
         long r = n;
         long count = 0;
         if(len==1 && s.equals("a")){
             return n;
         }
         while(r!=0){
-            if(s.charAt(d)=='a'){
+            if(s.charAt(count)=='a'){
                 count++;
 
             }
-            d=(d==len-1)?0:d+1;
+            count=(count==len-1)?0:count+1;
 
             if(r>0){
                 r--;
             }
 
+        }*/
+
+        // is efficient
+        int len = s.length();
+        long count = 0;
+        long rpt = n/len;
+        long c = n%len;
+        if(len==1 && s.equals("a")){
+            return n;
         }
+        for(int i =0;i<len;i++){
+            if(s.charAt(i)=='a'){
+                count++;
+            }
+        }
+        count*=rpt;
+        for (int i = 0; i < c; i++) {
+            if(s.charAt(i)=='a'){
+                count++;
+            }
+        }
+
+
+
        /* for(int i = 0; i<len && n-- > 0; i++){
             if(s.charAt(i) == 'a'){
-                count += d + (r>0? 1:0);
+                count += count + (r>0? 1:0);
             }
             if(r > 0){
                 r--;
